c714713866a3948f94d9e66b8231b237147a210e
Howligan <redrebel62@gmail.com>
2016-12-06 04:09:35 -0500

    Assigning cells to the Maze
    this helps with creating the maze further on. Episode 2 completed.

--- MazeGame/Assets/Scripts/Maze.cs
+++ MazeGame/Assets/Scripts/Maze.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class Maze : MonoBehaviour
 {
+    [System.Serializable]
+    public class Cell
+    {
+        public bool visited;
+        public GameObject North;
+        public GameObject East;
+        public GameObject West;
+        public GameObject South;
+    }
+
     public GameObject Wall;
     public int xSize = 5;
     public int ySize = 5;
     public float wallLength = 1f;
     private Vector3 initalPos;
     private GameObject wallHolder;
+    private Cell[] cells;
+
 
     void Start()
     {
@@ -44,6 +56,44 @@ public class Maze : MonoBehaviour
                 tempWall = Instantiate(Wall, myPos, Quaternion.Euler(0,90,0)) as GameObject;
                 tempWall.transform.parent = wallHolder.transform;
             }
+        }
+        CreateCells();
+    }
+
+    void CreateCells()
+    {
+        GameObject[] allWalls;
+        int children = wallHolder.transform.childCount;
+        allWalls = new GameObject[children];
+        cells = new Cell[xSize * ySize];
+        int eastWestProcess = 0;
+        int childProcess = 0;
+        int termCount = 0;
+
+
+        // Get all the children
+        for (int i = 0; i < children; i++)
+        {
+            allWalls[i] = wallHolder.transform.GetChild(i).gameObject;
+        }
+        // Assigns walls to the cells
+        for (int cellprocess = 0; cellprocess < cells.Length; cellprocess++)
+        {
+            cells[cellprocess] = new Cell ();
+            cells[cellprocess].East = allWalls[eastWestProcess];
+            cells[cellprocess].South = allWalls[childProcess + (xSize+1) * ySize];
+            if (termCount == xSize)
+            {
+                eastWestProcess += 2;
+                termCount = 0;
+            }
+            else
+                eastWestProcess++;
+
+            termCount++;
+            childProcess++;
+            cells[cellprocess].West = allWalls[eastWestProcess];
+            cells[cellprocess].North = allWalls[(childProcess + (xSize + 1) * ySize) + xSize-1];
         }
     }
 
